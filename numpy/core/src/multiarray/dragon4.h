efcc3603e056df7c708109a2c7f4310c999a7504
ShinYee <shinyee@speedgocomputing.com>
2018-08-30 23:00:04 +0800

    posit: Fix missing declaration.
    Signed-off-by: ShinYee <shinyee@speedgocomputing.com>

--- numpy/core/src/multiarray/dragon4.h
+++ numpy/core/src/multiarray/dragon4.h
@@ -121,6 +121,7 @@ make_dragon4_typedecl(Half, npy_half)
 make_dragon4_typedecl(Float, npy_float)
 make_dragon4_typedecl(Double, npy_double)
 make_dragon4_typedecl(LongDouble, npy_longdouble)
+make_dragon4_typedecl(Posit32, npy_posit32)
 
 #undef make_dragon4_typedecl
 
